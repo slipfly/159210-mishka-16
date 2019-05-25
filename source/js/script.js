a7e9b6be50fa91da27113fa378dabc20fb1ced69
slipfly <slipfly@gmail.com>
2019-05-26 00:04:51 +0300

    поправлены скрипты и отступы на главной

--- source/js/script.js
+++ source/js/script.js
@@ -10,7 +10,9 @@ pageHeader.classList.add('page-header--nav-closed');
 navToggle.classList.remove('visually-hidden');
 
 
-navToggle.addEventListener('click', function() {
+navToggle.addEventListener('click', function(evt) {
+  evt.preventDefault();
+
   if (pageHeader.classList.contains('page-header--nav-closed')) {
     pageHeader.classList.remove('page-header--nav-closed');
     pageHeader.classList.add('page-header--nav-opened');
@@ -20,14 +22,18 @@ navToggle.addEventListener('click', function() {
   }
 });
 
-orderBtn.addEventListener('click', function () {
+orderBtn.addEventListener('click', function (evt) {
+  evt.preventDefault();
+
   if (!modalBlock.classList.contains('modal__show')) {
     modalBlock.classList.add('modal__show');
   }
 });
 
 for (var i = 0; i < orderBtns.length; i++) {
-  orderBtns[i].addEventListener('click', function () {
+  orderBtns[i].addEventListener('click', function (evt) {
+    evt.preventDefault();
+
     if (!modalBlock.classList.contains('modal__show')) {
       modalBlock.classList.add('modal__show');
     }
